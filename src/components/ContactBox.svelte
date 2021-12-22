3b5464c7d7358bcb5fa2a071a06e4d1fd2541b7a
Sam Learner <learnersd@gmail.com>
2021-12-22 11:13:38 -0500

    Updated attribution

--- src/components/ContactBox.svelte
+++ src/components/ContactBox.svelte
@@ -1,90 +1,135 @@
 <script>
-    export let vizState;
-
+  export let vizState;
 </script>
 
+<div
+  style="display: {vizState === 'running' && window.innerWidth > 600
+    ? 'none'
+    : 'block'};"
+  class={"contact-box"}
+>
+  {#if window.innerWidth > 600}
+    <p>
+      <strong
+        >Visualization by <a
+          target="_blank"
+          href="https://bit.ly/main-project-site">Sam Learner</a
+        ></strong
+      >
+      |
+      <a target="_blank" href="https://twitter.com/sam_learner"
+        ><img
+          class="icon-img"
+          src="/images/twitter.svg"
+          alt="twitter link"
+        /></a
+      >
+      |
+      <a target="_blank" href="mailto:learnersd@gmail.com"
+        ><img class="icon-img" src="/images/email.svg" alt="email link" /></a
+      >
+      |
+      <a target="_blank" href="https://github.com/sdl60660"
+        ><img class="icon-img" src="/images/github.svg" alt="github link" /></a
+      >
+    </p>
+    <p>
+      <strong>Sources</strong>: The data used in this project comes from the
+      River Runner API, which is based on several open source projects and
+      datasets. Learn about it
+      <a href="https://ksonda.github.io/global-river-runner/" target="_blank"
+        >here</a
+      >. Code and data for this project lives
+      <a target="_blank" href={"https://github.com/sdl60660/river-runner/"}
+        >here</a
+      >.
+    </p>
+  {:else}
+    <strong
+      >Visualization by <a
+        target="_blank"
+        href="https://bit.ly/main-project-site">Sam Learner</a
+      ></strong
+    >
+    |
+    <a target="_blank" href="https://twitter.com/sam_learner"
+      ><img class="icon-img" src="/images/twitter.svg" alt="twitter link" /></a
+    >
+    |
+    <a target="_blank" href="mailto:learnersd@gmail.com"
+      ><img class="icon-img" src="/images/email.svg" alt="email link" /></a
+    >
+    |
+    <a target="_blank" href="https://github.com/sdl60660"
+      ><img class="icon-img" src="/images/github.svg" alt="github link" /></a
+    >
+    | Data:
+    <a href="https://ksonda.github.io/global-river-runner/" target="_blank"
+      >USGS/IoW</a
+    >.
+  {/if}
+</div>
+
 <style>
+  .contact-box {
+    background-color: rgba(243, 243, 243, 0.856);
+    padding: 1rem;
+    border-radius: 3px;
 
-    .contact-box {
-        background-color: rgba(243, 243, 243, 0.856);
-        padding: 1rem;
-        border-radius: 3px;
+    position: absolute;
+    z-index: 30;
+    font-size: 0.9rem;
+    bottom: 1.5rem;
+    right: 1rem;
+    max-width: 32rem;
 
-        position: absolute;
-        z-index: 30;
-        font-size: .9rem;
-        bottom: 1.5rem;
-        right: 1rem;
-        max-width: 32rem;
-
-        /* left: 50%;
+    /* left: 50%;
         transform: translateX(-50%);
         top: 1rem; */
 
-        /* display: flex;
+    /* display: flex;
         box-shadow: 4px 2px 2px rgba(56, 56, 56, 0.925); */
 
-        font-family: "Roboto","Inter", Arial, Helvetica, sans-serif;
-    }
+    font-family: "Roboto", "Inter", Arial, Helvetica, sans-serif;
+  }
 
-    .contact-box a {
-        text-decoration: none;
-    }
+  .contact-box a {
+    text-decoration: none;
+  }
 
-    .contact-box p {
-        margin-block-start: 0.7em;
-        margin-block-end: 0.7em;
-    }
+  .contact-box p {
+    margin-block-start: 0.7em;
+    margin-block-end: 0.7em;
+  }
 
-    .icon-img {
-        height: 14px;
-        width: 14px;
-        padding: 0px 2px;
-        fill: white;
-    }
+  .icon-img {
+    height: 14px;
+    width: 14px;
+    padding: 0px 2px;
+    fill: white;
+  }
 
-    a {
-        color: rgb(50, 133, 216)
-    }
+  a {
+    color: rgb(50, 133, 216);
+  }
 
-    @media only screen and (max-width: 600px) {
-        .contact-box {
-            box-sizing: border-box;
-            border-radius: 0;
-            right: unset;
-            bottom: 0;
-            padding: 5px;
-            background-color: white;
-            width: 100%;
-            text-align: center;
-            height: 1.5rem;
-        }
+  @media only screen and (max-width: 600px) {
+    .contact-box {
+      box-sizing: border-box;
+      border-radius: 0;
+      right: unset;
+      bottom: 0;
+      padding: 5px;
+      background-color: white;
+      width: 100%;
+      text-align: center;
+      height: 1.5rem;
     }
+  }
 
-    @media only screen and (max-width: 380px) {
-        .contact-box {
-            font-size: 0.85rem;
-        }
+  @media only screen and (max-width: 380px) {
+    .contact-box {
+      font-size: 0.85rem;
     }
-
+  }
 </style>
-
-<div style="display: {vizState === "running" && window.innerWidth > 600 ? "none" : "block"};" class={"contact-box"}>
-    {#if window.innerWidth > 600}
-        <p><strong>Visualization by <a target="_blank" href="https://bit.ly/main-project-site">Sam Learner</a></strong> |
-            <a target="_blank" href="https://twitter.com/sam_learner"><img class="icon-img" src="/images/twitter.svg" alt="twitter link"/></a> |
-            <a target="_blank" href="mailto:learnersd@gmail.com"><img class="icon-img" src="/images/email.svg" alt="email link"/></a> |
-            <a target="_blank" href="https://github.com/sdl60660"><img class="icon-img" src="/images/github.svg" alt="github link"/></a>
-        </p>
-        <p>
-            <strong>Sources</strong>: The data used in this project comes from the USGS's <a target="_blank" href="https://waterdata.usgs.gov/blog/nldi-intro/">NLDI API</a>, along with additional <a href="https://www.usgs.gov/core-science-systems/ngp/national-hydrography/nhdplus-high-resolution">NHDPlus</a> data.
-            Code and data for this project lives <a target="_blank" href={"https://github.com/sdl60660/river-runner/"}>here</a>.
-        </p>
-    {:else}
-        <strong>Visualization by <a target="_blank" href="https://bit.ly/main-project-site">Sam Learner</a></strong> |
-        <a target="_blank" href="https://twitter.com/sam_learner"><img class="icon-img" src="/images/twitter.svg" alt="twitter link"/></a> |
-        <a target="_blank" href="mailto:learnersd@gmail.com"><img class="icon-img" src="/images/email.svg" alt="email link"/></a> |
-        <a target="_blank" href="https://github.com/sdl60660"><img class="icon-img" src="/images/github.svg" alt="github link"/></a> | 
-        Data: <a href="https://www.usgs.gov/core-science-systems/ngp/national-hydrography/nhdplus-high-resolution">USGS</a>.
-    {/if}
-</div>
