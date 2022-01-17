ee865de97971d5724ac81c9c33d5535cf0e4205c
sdl60660 <learnersd@gmail.com>
2022-01-17 11:38:02 -0500

    Updated overrides based on suggestions

--- name_server/data_processing/aggregate_name_suggestions.js
+++ name_server/data_processing/aggregate_name_suggestions.js
@@ -71,7 +71,7 @@ const runAggregation = async (aggregationPipeline, collection) => {
 
         const coll = client.db("river_runner").collection(collection);
 
-        coll.aggregate(aggregationPipeline, async (cmdErr, result) => {
+        coll.aggregate(aggregationPipeline, { allowDiskUse: true, cursor: { batchSize: 1000 } }, async (cmdErr, result) => {
           assert.equal(null, cmdErr);
 
           const aggregationData = await result.toArray();
@@ -88,43 +88,43 @@ const main = async () => {
   const csvFilepath = "../../public/data/name_overrides.csv";
 
   const existingOverrides = await getExistingOverrides(csvFilepath);
-  const groupedOccurences = await runAggregation(unnamedFeatureCounts, "unnamed_features");
+  // const groupedOccurences = await runAggregation(unnamedFeatureCounts, "unnamed_features");
   const groupedSuggestions = await runAggregation(suggestionCounts, "suggestions");
 
-  //   const overridenIDs = existingOverrides.map((d) => Number(d.levelpathid));
-  //   const unhandledSuggestions = groupedSuggestions
-  //     .filter((d) => d.count > 3)
-  //     .filter((d) => !overridenIDs.includes(d._id));
+  const overridenIDs = existingOverrides.map((d) => Number(d.levelpathid));
+  const unhandledSuggestions = groupedSuggestions
+    .filter((d) => d.count > 3)
+    .filter((d) => !overridenIDs.includes(d._id));
 
-  //   console.log(unhandledSuggestions);
-  const joinedData = groupedOccurences.map((d) => {
-    const suggestions = groupedSuggestions.find((a) => a._id === d._id) || {
-      count: 0,
-      route_url: [],
-      suggestions: null,
-    };
+  console.log(unhandledSuggestions);
+  // const joinedData = groupedOccurences.map((d) => {
+  //   const suggestions = groupedSuggestions.find((a) => a._id === d._id) || {
+  //     count: 0,
+  //     route_url: [],
+  //     suggestions: null,
+  //   };
 
-    const override = existingOverrides.find((a) => Number(a.levelpathid) === d._id);
+  //   const override = existingOverrides.find((a) => Number(a.levelpathid) === d._id);
 
-    return {
-      levelpathid: d._id,
-      sample_route_url: [...suggestions.route_url, d.route_url].filter((d) => d)[0],
-      num_suggestions: suggestions.count,
-      num_occurences: d.count,
-      suggestions: suggestions.suggestions,
-      overriden: override ? true : false,
-      overriden_val: override ? override.feature_name : null,
-    };
-  });
+  //   return {
+  //     levelpathid: d._id,
+  //     sample_route_url: [...suggestions.route_url, d.route_url].filter((d) => d)[0],
+  //     num_suggestions: suggestions.count,
+  //     num_occurences: d.count,
+  //     suggestions: suggestions.suggestions,
+  //     overriden: override ? true : false,
+  //     overriden_val: override ? override.feature_name : null,
+  //   };
+  // });
 
-  const outputFile = fs.createWriteStream("data/aggregated_suggestions.csv");
+  // const outputFile = fs.createWriteStream("data/aggregated_suggestions.csv");
 
-  csv
-    .write(joinedData, { headers: true })
-    .on("finish", function () {
-      console.log("Write to CSV successfully!");
-    })
-    .pipe(outputFile);
+  // csv
+  //   .write(joinedData, { headers: true })
+  //   .on("finish", function () {
+  //     console.log("Write to CSV successfully!");
+  //   })
+  //   .pipe(outputFile);
 };
 
 main();
